ca08514e6001aba9fcb12c65cd8b217681c19b03
Tobore Oghenevovwerho <oghenevovwerho.iruo@gmail.com>
2026-03-17 17:08:12 +0100

    added star

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    safelist: ["*"],
+    theme: {
+        extend: {}, // Add your custom theme customizations here
+    },
+    plugins: [], // Add Tailwind plugins here (e.g., DaisyUI, Typography)
+};
